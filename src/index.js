11c936b09432fb71d3a4dca630e8b33379418af1
achernyi <ote1188@gmail.com>
2018-12-01 17:39:11 +0200

    Added the video selection

--- src/index.js
+++ src/index.js
@@ -1,18 +1,39 @@
 //Create a new component. This component should produce some HTML
 
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from "react-dom";
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from  "./components/search_bar";
+import VideoList from  "./components/video_list";
+import VideoDetail from  "./components/video_detail";
 
 const API_KEY_YOUTUBE = "AIzaSyBFyx5H3waXZMkCS4ymZQwZMVb4uWzZVq0";
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar/>
+
+class App extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {videos: [] , selectedVideo: null};
+
+        YTSearch({key:API_KEY_YOUTUBE, term: "dinamo kiev"}, (videos) => {
+            this.setState({videos : videos, selectedVideo: videos[0]}); // {videos : videos} //ES6
+        });
+
+    }
+
+
+
+    render (){
+        return <div>
+        <SearchBar/>
+        <VideoDetail video = {this.state.selectedVideo}/>
+        <VideoList
+            onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+            videos = {this.state.videos}/>
         </div>
-);
+    }
 }
 
 
