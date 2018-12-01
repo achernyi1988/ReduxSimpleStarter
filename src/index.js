edd1985c661694ef7fac6c412e57809c4cfbff57
achernyi <achernyi@luxoft.com>
2018-12-01 14:18:20 +0200

    Added components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
+//Create a new component. This component should produce some HTML
+
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import ReactDOM from "react-dom";
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from  "./components/search_bar";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY_YOUTUBE = "AIzaSyBFyx5H3waXZMkCS4ymZQwZMVb4uWzZVq0";
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const App = () => {
+    return (
+        <div>
+            <SearchBar/>
+        </div>
+);
+}
+
+
+
+
+//Take this component's generated HTML and put it on the page (in the DOM)
+
+ReactDOM.render(<App/>, document.querySelector(".container"));
